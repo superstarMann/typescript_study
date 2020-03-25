d9af7387d3c59bb87867dc2e7aec1a9d75571e69
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2020-03-25 12:42:56 +0900

    index.ts 를 src로 넣어야된단다

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Seunghun", color = "red", adress = "seoul";
+const introduce = (name, color, adress) => {
+    console.log(`i'm ${name}, my favorite color is ${color}, and i live in ${adress} `);
+};
+introduce(name, color, adress);
+const wow = (name, age) => {
+    return (`i am ${name}, i'm ${age}`);
+};
+wow("seunghun", 23);
+//# sourceMappingURL=index.js.map
