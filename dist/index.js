8cfd7267c5fc459a664a9dbfb2f9d1c23025b8b3
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2020-03-28 17:00:07 +0900

    2020.03.28

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const CryptoJS = require("crypto-js");
 class Block {
     constructor(index, hash, previousHash, data, timestamp) {
         this.index = index;
@@ -9,6 +10,7 @@ class Block {
         this.timestamp = timestamp;
     }
 }
+Block.calculateBlockhash = (index, previousHash, data, timestamp) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 const genesisBlock = new Block(0, "2020202020", "", "wow", 1234);
 let BlockChain = [genesisBlock];
 console.log(BlockChain);
