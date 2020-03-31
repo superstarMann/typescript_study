5b60fa62eee94d0a946805ccc4d30bfef2064da2
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2020-03-31 14:22:16 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,8 @@ class Block{
     static calculateBlockhash = (index: number, previousHash: string, data: string, timestamp: number): string =>
     CryptoJS.SHA256(index + previousHash + timestamp + data).toString(); 
 
+    static validateStructure = (aBlock: Block) : boolean => typeof aBlock.index === "number" && typeof aBlock.hash === "string" && typeof aBlock.previousHash === "string";
+
     constructor (index: number, hash: string, previousHash: string, data: string, timestamp: number){
         this.index = index;
         this.hash = hash;
@@ -19,10 +21,29 @@ class Block{
     }
 }
 
+
+
 const genesisBlock: Block = new Block( 0, "2020202020", "", "wow", 1234);
 
 let BlockChain: Block[]= [genesisBlock];
 
-console.log(BlockChain);
+const getBlockchain = () : Block[] => BlockChain
+
+const getLatestBlock =() : Block => BlockChain[BlockChain.length -1];
+
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data:string) : Block => {
+    const previosBlock: Block = getLatestBlock();
+    const newIndex: number = previosBlock.index + 1;
+    const newTimestamp: number =getNewTimeStamp();
+    const newHash: string = Block.calculateBlockhash(newIndex, previosBlock.hash, nextTimestamp, data);
+    const newBlock : Block = new Block(newIndex,previosBlock.hash , newHash, data, newTimestamp);
+    return newBlock;
+};
+
+const isBlockValid = (candidateBlock: Block, previosBlock: Block) : boolean => {
+    if()
+}
 
 export{};
